09ddcb9c771005c76f60565aa35161477aa0a97a
ItsJokerZz <ItsJokerZz@Gmail.com>
2025-03-01 12:21:16 -0500

    Better Handle File Downloads

--- source/plugin/headers/http.hpp
+++ source/plugin/headers/http.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
 extern bool threadDownload;
-extern const char *fileName;
+extern char *fileName;
 extern int downloadProgress;
 extern int64_t totalFileSize;
 extern int64_t currentSize;
+extern double downloadSpeed;
 extern char *dataBuffer;
 extern size_t bufferSize;
-extern long httpStatusCode;
-
+extern char *filePath;
+extern bool hasDownloadCompleted;
+extern bool downloadErrorOccured;
 extern std::atomic<bool> cancelDownload;
+extern CURL *curl;
 
 static int UpdateDownloadProgress(void *bar, int64_t totalSize, int64_t downloadedSize, int64_t, int64_t);
 void BeginDownload(const char *url, const char *pathWithFile);
@@ -17,10 +20,11 @@ static size_t DownloadDataCallback(void *ptr, size_t size, size_t nmemb);
 
 extern "C"
 {
+    bool HasDownloadCompleted();
+    bool HasDownloadErrorOccured();
     void ResetDownloadVars();
     void DownloadWebFile(const char *url, const char *pathWithFile, bool bgDL, const char *name);
     char *DownloadAsBytes(const char *url, size_t *out_size);
     char *GetDownloadInfo(const char *info);
-    long GetDownloadError();
     void CancelDownload();
 }
--- source/plugin/source/http.cpp
+++ source/plugin/source/http.cpp
@@ -1,54 +1,103 @@
 #include "../headers/includes.hpp"
 
 bool threadDownload = true;
-const char *fileName = NULL;
+char *fileName = nullptr;
 int downloadProgress = 0;
 int64_t totalFileSize = 0;
 int64_t currentSize = 0;
-char *dataBuffer = NULL;
+double downloadSpeed = 0.0;
+char *dataBuffer = nullptr;
 size_t bufferSize = 0;
-long httpStatusCode = 0;
-
+char *filePath = nullptr;
+bool hasDownloadCompleted = false;
+bool downloadErrorOccured = false;
 std::atomic<bool> cancelDownload(false);
+CURL *curl = nullptr;
+
+bool HasDownloadCompleted()
+{
+    return hasDownloadCompleted;
+}
+
+bool HasDownloadErrorOccured()
+{
+    return downloadErrorOccured;
+}
 
 void ResetDownloadVars()
 {
     cancelDownload.store(false);
-
     downloadProgress = 0;
     totalFileSize = 0;
     currentSize = 0;
-    fileName = nullptr;
+    downloadSpeed = 0.0;
 
-    if (dataBuffer)
+    if (fileName)
     {
-        free(dataBuffer);
-        dataBuffer = nullptr;
+        free(fileName);
+        fileName = nullptr;
     }
 
+    free(dataBuffer);
+    dataBuffer = nullptr;
     bufferSize = 0;
-    httpStatusCode = 0;
+
+    if (curl)
+    {
+        curl_easy_cleanup(curl);
+        curl = nullptr;
+    }
+
+    hasDownloadCompleted = false;
+    downloadErrorOccured = false;
 }
 
-static int UpdateDownloadProgress(void *bar, int64_t totalSize, int64_t downloadedSize, int64_t, int64_t)
+char *GetDownloadInfo(const char *info)
+{
+    if (strcmp(info, "speed") == 0)
+        return strdup(std::to_string(downloadSpeed).c_str());
+    else if (strcmp(info, "downloaded") == 0)
+        return strdup(std::to_string(currentSize).c_str());
+    else if (strcmp(info, "filesize") == 0)
+        return strdup(std::to_string(totalFileSize).c_str());
+    else if (strcmp(info, "progress") == 0)
+        return strdup(std::to_string(downloadProgress).c_str());
+    return nullptr;
+}
+
+void CancelDownload()
 {
     if (cancelDownload.load())
-        return CURLE_ABORTED_BY_CALLBACK; // Signal to CURL to stop the download
+        cancelDownload.store(false);
 
-    int progress = 0;
-    if (totalSize > 0)
-        progress = static_cast<int>((static_cast<float>(downloadedSize) / totalSize) * 100.f);
+    cancelDownload.store(true);
+}
 
-    downloadProgress = progress;
-    totalFileSize = totalSize;
-    currentSize = downloadedSize;
+static int UpdateDownloadProgress(void *, int64_t totalSize, int64_t downloadedSize, int64_t, int64_t)
+{
+    if (cancelDownload.load())
+        return CURLE_ABORTED_BY_CALLBACK;
 
-    if (!threadDownload)
+    if (curl)
     {
-        if (fileName == NULL || strcmp(fileName, "NULL") == 0)
-            setProgressMsgText(downloadProgress, "Downloading...");
-        else
-            setProgressMsgText(downloadProgress, "Downloading \"%s\"", fileName);
+        double currentDownloadSpeed = 0.0;
+        CURLcode speedResult = curl_easy_getinfo(curl, CURLINFO_SPEED_DOWNLOAD, &currentDownloadSpeed);
+
+        if (speedResult == CURLE_OK)
+        {
+            downloadSpeed = currentDownloadSpeed;
+            totalFileSize = totalSize;
+            currentSize = downloadedSize;
+            downloadProgress = totalSize < 0 ? 0 : static_cast<int>((static_cast<float>(downloadedSize) / totalSize) * 100.f);
+        }
+
+        if (!threadDownload)
+        {
+            if (!fileName || strcmp(fileName, "NULL") == 0)
+                setProgressMsgText(downloadProgress, "Downloading...");
+            else
+                setProgressMsgText(downloadProgress, "Downloading \"%s\"", fileName);
+        }
     }
 
     return 0;
@@ -58,32 +107,44 @@ void BeginDownload(const char *url, const char *pathWithFile)
 {
     ResetDownloadVars();
 
+    if (!pathWithFile)
+    {
+        PrintAndLog("Download path is null.", 3);
+        downloadErrorOccured = true;
+        return;
+    }
+
     FILE *file = fopen(pathWithFile, "wb");
     if (!file)
     {
-        printAndLog(3, "Failed to open file... %s due to: %s", pathWithFile, strerror(errno));
+        PrintAndLog(("Failed to open file: " + std::string(pathWithFile) + " due to: " + strerror(errno)).c_str(), 3);
+        downloadErrorOccured = true;
+        return;
+    }
 
+    filePath = strdup(pathWithFile);
+    if (!filePath)
+    {
+        PrintAndLog("Memory allocation failed for filePath.", 3);
+        downloadErrorOccured = true;
         fclose(file);
-
         return;
     }
 
     if (!threadDownload)
         initiateProgressDialog((char *)"Starting download...");
 
-    CURL *curl = curl_easy_init();
+    curl = curl_easy_init();
     if (!curl)
     {
-        printAndLog(3, "CURL has failed to initialize.");
-
-        fclose(file); // Close the file if CURL initialization fails
-
-        httpStatusCode = -1;
-        unlink(pathWithFile); // Delete the file after closing
-
+        PrintAndLog("CURL failed to initialize.", 3);
+        downloadErrorOccured = true;
+        fclose(file);
+        unlink(filePath);
+        free(filePath);
+        filePath = nullptr;
         if (!threadDownload)
             sceMsgDialogTerminate();
-
         return;
     }
 
@@ -104,32 +165,29 @@ void BeginDownload(const char *url, const char *pathWithFile)
     curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
 
     CURLcode result = curl_easy_perform(curl);
-
     if (result != CURLE_OK)
     {
-        printAndLog(result == 42 ? 2 : 3, curl_easy_strerror(result));
-
-        httpStatusCode = -1;
-        unlink(pathWithFile);
-    }
-    else
-    {
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpStatusCode);
-
-        if (httpStatusCode == 200)
-            printAndLog(1, "Download completed with HTTP response code: %ld", httpStatusCode);
+        if (result == CURLE_ABORTED_BY_CALLBACK)
+            PrintAndLog(std::string(curl_easy_strerror(result)).c_str(), 2);
         else
         {
-            printAndLog(1, "Download failed with HTTP response code: %ld, deleting file...", httpStatusCode);
-
-            httpStatusCode = -1;
-            unlink(pathWithFile);
+            PrintAndLog(("Download failed with error: " + std::string(curl_easy_strerror(result))).c_str(), 3);
+            downloadErrorOccured = true;
         }
+
+        unlink(filePath);
     }
+    else
+        PrintAndLog("Download completed successfully.", 1);
+
+    hasDownloadCompleted = true;
 
     curl_easy_cleanup(curl);
+    curl = nullptr;
 
     fclose(file);
+    free(filePath);
+    filePath = nullptr;
 
     if (!threadDownload)
         sceMsgDialogTerminate();
@@ -138,7 +196,17 @@ void BeginDownload(const char *url, const char *pathWithFile)
 void DownloadWebFile(const char *url, const char *pathWithFile, bool bgDL, const char *name)
 {
     threadDownload = bgDL;
-    fileName = name;
+
+    if (!url || !pathWithFile)
+    {
+        PrintAndLog("Invalid URL or file path.", 3);
+        return;
+    }
+
+    if (fileName)
+        free(fileName);
+
+    fileName = name ? strdup(name) : nullptr;
 
     if (!bgDL)
         BeginDownload(url, pathWithFile);
@@ -146,19 +214,14 @@ void DownloadWebFile(const char *url, const char *pathWithFile, bool bgDL, const
     {
         std::string urlCopy(url);
         std::string filePathCopy(pathWithFile);
-
         std::thread downloadThread([urlCopy, filePathCopy]()
                                    { BeginDownload(urlCopy.c_str(), filePathCopy.c_str()); });
 
         downloadThread.detach();
     }
 
-    if (bgDL)
-        printAndLog(1, "Background download has begun.", url, pathWithFile, bgDL);
-    else
-        printAndLog(1, "Foreground download has begun.", url, pathWithFile, bgDL);
-
-    printAndLog(0, "Downloading file and writing to: %s", pathWithFile);
+    PrintAndLog(bgDL ? "Background download has begun." : "Foreground download has begun.", 1);
+    PrintAndLog(("Downloading file and writing to: " + std::string(pathWithFile)).c_str(), 1);
 }
 
 static size_t DownloadDataCallback(void *ptr, size_t size, size_t count)
@@ -187,7 +250,6 @@ char *DownloadThread(const char *url, size_t *out_size)
     CURL *curl = curl_easy_init();
     if (!curl)
     {
-        httpStatusCode = -1;
         *out_size = 0;
         return NULL;
     }
@@ -212,7 +274,6 @@ char *DownloadThread(const char *url, size_t *out_size)
         free(dataBuffer);
         dataBuffer = NULL;
         bufferSize = 0;
-        httpStatusCode = static_cast<long>(result);
         curl_easy_cleanup(curl);
         return NULL;
     }
@@ -231,42 +292,4 @@ char *DownloadAsBytes(const char *url, size_t *out_size)
                                             });
 
     return future.get();
-}
-
-char *GetDownloadInfo(const char *info)
-{
-    static char buffer[128];
-
-    struct
-    {
-        const char *key;
-        int64_t *value;
-        const char *format;
-    } infoMap[] = {
-        {"downloaded", &currentSize, "%" PRId64},
-        {"filesize", &totalFileSize, "%" PRId64},
-        {"progress", (int64_t *)&downloadProgress, "%d"},
-    };
-
-    for (const auto &entry : infoMap)
-    {
-        if (strcmp(info, entry.key) == 0)
-        {
-            snprintf(buffer, sizeof(buffer), entry.format, *entry.value);
-
-            return buffer;
-        }
-    }
-
-    return NULL;
-}
-
-long GetDownloadError()
-{
-    return httpStatusCode;
-}
-
-void CancelDownload()
-{
-    cancelDownload.store(true);
 }
